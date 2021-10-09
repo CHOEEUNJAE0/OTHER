7d7e712ba17f61cc636a9bedc75ee40473b64f07
JAEJAE <onbandal@gmail.com>
2021-10-09 18:10:12 +0900

    연산자

--- Javastudy/src/com/jv/exam/Operator.java
+++ Javastudy/src/com/jv/exam/Operator.java
@@ -3,6 +3,8 @@ package com.jv.exam;
 public class Operator {
 
 	public static void main(String[] args) {
+		//https://slow-magnolia-307.notion.site/Operator-6a9304eae595414fb8bff87b035b9b20
+		
 		/*
 		 *  대입 연산자와 산술 연산자
 		 *  = : 연산자 오른쪽에 있는 값을 왼쪽에 있는 변수에 대입
@@ -74,7 +76,28 @@ public class Operator {
 		System.out.println(result);
 		
 		
+		/*
+		 * 증가 감소 연산자 : ++, --
+		 * 피연산자의 값에 1을 더하거나 빼는 연산자로 위치에 따라 결과 값이 다르게 나타난다.
+		 * 전위연산자 : 먼저 연산 후 다른 연산을 실행 ++i
+		 * (i를 증가 시킨 후 값을 활용)
+		 * 후위연산자 : 다른 연산 우선 실행 후 연산을 실행 i++
+		 * (i가 가지고 있는 값을 문장에서 먼저 활용을 하고 후에 증가시켜라)
+		 */
 		
+		int val = 7;
+		System.out.println(++val);
+		System.out.println(++val);
+		System.out.println(val);
+		
+		int val1 =5;
+		System.out.print((val1++) + " ");//출력 후 값이 증가
+		System.out.print((val1++) + " ");//출력 후 값이 증가
+		System.out.print(val1 + "\n"); // 증가 한 값은 다음 문장으로 넘어가야 확인 할 수 있다.
+		
+		System.out.print((val1--) + " ");//출력 후 값이 감소
+		System.out.print((val1-- ) + " ");//출력 후 값이 감소
+		System.out.print(val1 + "\n"); // 감소ㅓ한 값은 다음 문장으로 넘어가야 확인 할 
 
 	}
 
