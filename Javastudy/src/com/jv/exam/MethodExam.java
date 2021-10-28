bbcf668a16219564f027ce2da3db3afba85d6bee
JAEJAE <onbandal@gmail.com>
2021-10-28 19:12:24 +0900

    메소드의 정의와 호출

---
+++ Javastudy/src/com/jv/exam/MethodExam.java
@@ -0,0 +1,21 @@
+package com.jv.exam;
+
+public class MethodExam {
+	
+	//
+	public static void numbering() {
+		
+		int i = 0;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		//numberingÀÌ¶ó´Â ¸Þ¼­µå°¡ È£­„ µÈ´Ù.
+		numbering();
+	}
+
+}
